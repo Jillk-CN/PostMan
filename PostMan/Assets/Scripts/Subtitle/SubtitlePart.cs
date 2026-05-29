0a0c4b6f3108b749b448347a076689a0baa67ae2
HyViques <88628593+HyViques@users.noreply.github.com>
2026-05-29 11:27:03 +0800

    字幕片段记录类
    补充一个可用于记录一个字幕片段的起始索引和结束索引的类

---
+++ PostMan/Assets/Scripts/Subtitle/SubtitlePart.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 用于记录一个字幕片段的起始索引和结束索引
+/// 可被用于需要重复使用去播放多段不同字幕的脚本，如用List<SubtitlePart>
+/// 若只播放一句字幕，则起始结束索引都填相同
+/// 可被序列化
+/// </summary>
+[Serializable]
+public class SubtitlePart
+{
+    int startIndex;
+    int endIndex;
+}
